5834971adf76c95f5244f23fd4ba9ac87f311d25
Peder Aursand <peder.aursand@nr.no>
2017-03-20 14:56:37 +0100

    Fix for list retention issue

--- app/src/main/java/net/ddns/peder/drevet/adapters/LandmarksCursorAdapter.java
+++ app/src/main/java/net/ddns/peder/drevet/adapters/LandmarksCursorAdapter.java
@@ -6,17 +6,12 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.widget.AppCompatCheckBox;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import net.ddns.peder.drevet.R;
 import net.ddns.peder.drevet.database.LandmarksDbHelper;
-
-import java.util.ArrayList;
 
 /**
  * Created by peder on 3/19/17.
@@ -27,8 +22,6 @@ public class LandmarksCursorAdapter extends SimpleCursorAdapter {
     private int item_layout;
     private SQLiteDatabase db;
     private LandmarksDbHelper dbHelper;
-    private ArrayList<Boolean> isShowedChecked = new ArrayList<>();
-    private ArrayList<Boolean> isSharedChecked = new ArrayList<>();
 
     public LandmarksCursorAdapter (Context context, int layout, Cursor cursor, String[] fromCols,
                                             int[] toViews) {
@@ -38,53 +31,6 @@ public class LandmarksCursorAdapter extends SimpleCursorAdapter {
         this.mContext = context;
         dbHelper = new LandmarksDbHelper(context);
         db = dbHelper.getReadableDatabase();
-        for (int i=0; i< this.getCount(); i++) {
-            isShowedChecked.add(i, false);
-            isSharedChecked.add(i, false);
-        }
-    }
-
-    @Override
-    public View getView(final int position, View inView, ViewGroup parent) {
-        if (inView == null) {
-            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(
-                                                            Context.LAYOUT_INFLATER_SERVICE);
-            inView = inflater.inflate(R.layout.lm_row, null);
-        }
-
-        final AppCompatCheckBox show = (AppCompatCheckBox) inView.findViewById(R.id.show_check_box);
-
-        final AppCompatCheckBox share = (AppCompatCheckBox) inView.findViewById(R.id.share_check_box);
-
-        show.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AppCompatCheckBox cb = (AppCompatCheckBox) view.findViewById(R.id.show_check_box);
-                if (cb.isChecked()) {
-                    isShowedChecked.set(position, true);
-                }
-                else if (!cb.isChecked()) {
-                    isShowedChecked.set(position, false);
-                }
-            }
-        });
-        show.setChecked(isShowedChecked.get(position));
-
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AppCompatCheckBox cb = (AppCompatCheckBox) view.findViewById(R.id.share_check_box);
-                if (cb.isChecked()) {
-                    isSharedChecked.set(position, true);
-                }
-                else if (!cb.isChecked()) {
-                    isSharedChecked.set(position, false);
-                }
-            }
-        });
-        share.setChecked(isSharedChecked.get(position));
-
-        return inView;
     }
 
     @Override
@@ -98,11 +44,12 @@ public class LandmarksCursorAdapter extends SimpleCursorAdapter {
         descriptionView.setText(description);
 
         AppCompatCheckBox show = (AppCompatCheckBox) view.findViewById(R.id.show_check_box);
-        show.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        show.setOnClickListener(new View.OnClickListener() {
              @Override
-             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+             public void onClick(View v) {
+                 AppCompatCheckBox show = (AppCompatCheckBox) v.findViewById(R.id.show_check_box);
                  ContentValues values = new ContentValues();
-                 if (isChecked) {
+                 if (show.isChecked()) {
                      values.put(LandmarksDbHelper.COLUMN_NAME_SHOWED, 1);
                  }
                  else {
@@ -110,16 +57,18 @@ public class LandmarksCursorAdapter extends SimpleCursorAdapter {
                  }
                  db.update(LandmarksDbHelper.TABLE_NAME, values, LandmarksDbHelper.COLUMN_NAME_ID+" = ?",
                                                         new String[]{Long.toString(itemId)});
+                 cursor.requery();
              }
         }
         );
 
         AppCompatCheckBox share = (AppCompatCheckBox) view.findViewById(R.id.share_check_box);
-        share.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        share.setOnClickListener(new View.OnClickListener() {
              @Override
-             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+             public void onClick(View v) {
+                 AppCompatCheckBox share = (AppCompatCheckBox) v.findViewById(R.id.share_check_box);
                  ContentValues values = new ContentValues();
-                 if (isChecked) {
+                 if (share.isChecked()) {
                      values.put(LandmarksDbHelper.COLUMN_NAME_SHARED, 1);
                  }
                  else {
@@ -127,9 +76,11 @@ public class LandmarksCursorAdapter extends SimpleCursorAdapter {
                  }
                  db.update(LandmarksDbHelper.TABLE_NAME, values, LandmarksDbHelper.COLUMN_NAME_ID+" = ?",
                                                         new String[]{Long.toString(itemId)});
+                 cursor.requery();
              }
         }
         );
+
         // Set show checkbox
         final boolean isShowed = cursor.getInt(cursor.getColumnIndex(
                 LandmarksDbHelper.COLUMN_NAME_SHOWED)) > 0;
@@ -139,8 +90,6 @@ public class LandmarksCursorAdapter extends SimpleCursorAdapter {
         final boolean isShared = cursor.getInt(cursor.getColumnIndex(
                 LandmarksDbHelper.COLUMN_NAME_SHARED)) > 0;
         share.setChecked(isShared);
-
-
 
         ImageButton deleteButton = (ImageButton) view.findViewById(R.id.lm_delete_button);
         deleteButton.setOnClickListener(new View.OnClickListener(){
