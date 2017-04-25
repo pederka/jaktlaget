18077c39c69be557d5be6d5eaf5bdb2097a0c27d
Peder Aursand <peder.aursand@nr.no>
2017-04-25 15:16:09 +0200

    Splash screen

---
+++ app/src/main/java/net/ddns/peder/drevet/SplashActivity.java
@@ -0,0 +1,23 @@
+package net.ddns.peder.drevet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by peder on 25.04.2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+}
+
