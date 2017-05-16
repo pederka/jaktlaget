6dbc18c0604200ccc2e24cb3b1d387a51bca3be6
Peder Aursand <peder.aursand@nr.no>
2017-05-16 21:55:51 +0200

    Add confirmation dialog to team reset button

--- app/src/main/java/net/ddns/peder/jaktlaget/fragments/TeamInfoFragment.java
+++ app/src/main/java/net/ddns/peder/jaktlaget/fragments/TeamInfoFragment.java
@@ -1,6 +1,7 @@
 package net.ddns.peder.jaktlaget.fragments;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,7 +48,7 @@ public class TeamInfoFragment extends Fragment {
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
         String userId = prefs.getString(Constants.SHARED_PREF_USER_ID, Constants.DEFAULT_USER_ID);
         // Read teamid from preferences
-        String teamId = prefs.getString(Constants.SHARED_PREF_TEAM_ID, Constants.DEFAULT_TEAM_ID);
+        final String teamId = prefs.getString(Constants.SHARED_PREF_TEAM_ID, Constants.DEFAULT_TEAM_ID);
         String code = prefs.getString(Constants.SHARED_PREF_TEAM_CODE, "");
 
         // Set text information
@@ -61,32 +63,46 @@ public class TeamInfoFragment extends Fragment {
         resetTeamButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Remove team and code from shared preferences
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
-                            getContext());
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putString(Constants.SHARED_PREF_USER_ID, Constants.DEFAULT_USER_ID);
-                editor.putString(Constants.SHARED_PREF_TEAM_ID, Constants.DEFAULT_TEAM_ID);
-                editor.putString(Constants.SHARED_PREF_TEAM_CODE, "");
-                editor.apply();
-                // Clear database entries
-                PositionsDbHelper positionsDbHelper = new PositionsDbHelper(getContext());
-                TeamLandmarksDbHelper teamLandmarksDbHelper = new TeamLandmarksDbHelper(getContext());
-                SQLiteDatabase posdb = positionsDbHelper.getWritableDatabase();
-                SQLiteDatabase lmdb = teamLandmarksDbHelper.getReadableDatabase();
-                positionsDbHelper.clearTable(posdb);
-                teamLandmarksDbHelper.clearTable(lmdb);
-                posdb.close();
-                lmdb.close();
-                // Cancel any position services
-                if (((MainActivity)getActivity()).isActive()) {
-                    ((MainActivity)getActivity()).goInactive();
-                }
-                // Go to team management fragment
-                Fragment fragment = new TeamManagementFragment();
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.replace(R.id.content_frame, fragment);
-                ft.commit();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle(R.string.reset_team_dialog);
+                builder.setMessage(String.format(getString(R.string.reset_team_text), teamId));
+                builder.setPositiveButton(R.string.reset_team_button, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Remove team and code from shared preferences
+                        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
+                                    getContext());
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString(Constants.SHARED_PREF_USER_ID, Constants.DEFAULT_USER_ID);
+                        editor.putString(Constants.SHARED_PREF_TEAM_ID, Constants.DEFAULT_TEAM_ID);
+                        editor.putString(Constants.SHARED_PREF_TEAM_CODE, "");
+                        editor.apply();
+                        // Clear database entries
+                        PositionsDbHelper positionsDbHelper = new PositionsDbHelper(getContext());
+                        TeamLandmarksDbHelper teamLandmarksDbHelper = new TeamLandmarksDbHelper(getContext());
+                        SQLiteDatabase posdb = positionsDbHelper.getWritableDatabase();
+                        SQLiteDatabase lmdb = teamLandmarksDbHelper.getReadableDatabase();
+                        positionsDbHelper.clearTable(posdb);
+                        teamLandmarksDbHelper.clearTable(lmdb);
+                        posdb.close();
+                        lmdb.close();
+                        // Cancel any position services
+                        if (((MainActivity)getActivity()).isActive()) {
+                            ((MainActivity)getActivity()).goInactive();
+                        }
+                        // Go to team management fragment
+                        Fragment fragment = new TeamManagementFragment();
+                        FragmentTransaction ft = getFragmentManager().beginTransaction();
+                        ft.replace(R.id.content_frame, fragment);
+                        ft.commit();
+                    }
+                });
+                builder.setNegativeButton(R.string.lm_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
         return view;
